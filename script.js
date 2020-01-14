d28bb383e90c3a83567bc0ae38a4365b5abe708c
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-01-14 12:19:58 +0100

    Created script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+1. Create an array called petArray with 3 string items in it: "cat", "cow", "dog".
+
+
+2. Console log the value of the second item in petArray.
+
+
+3. Add the item "sheep" to petArray using the push method.
+
+
+4. Console log how many items there are now in petArray using the length method.
+
+5. Create an object called catObject with 3 properties: name, colour, age. Add appropriate values (and type of values) for each of these properties.
+
+
+6. Console log the value of the third property of catObject.
+
+7. Take the value of catObject (the {} part) and place it inside an array called catArray.
+
+
+8. Loop throughcatArray and console log each property of each object inside (there will be only 1 object inside).
+
+
+9. Create a function called logToConsole. Th function should accept 1 argument. Decide on the name of the argument and console log its value inside the function.
+
+
+10. Call the logToConsole function and pass in the value "donkey".
