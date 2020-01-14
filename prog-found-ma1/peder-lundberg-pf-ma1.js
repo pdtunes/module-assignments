fc40417d9370259152ecd70500576d9cf3f9488c
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-01-14 13:53:55 +0100

    added last week and changed name

---
+++ prog-found-ma1/peder-lundberg-pf-ma1.js
@@ -0,0 +1,29 @@
+/* 1. Declare a variable called firstVar but don't initialise it with a value.*/
+var firstvar;
+
+/* 2. Declare a variable called name and assign it your first name.*/
+var name = "Peder";
+
+/* 3 Declare and initialise a variable with a number value.*/
+var numbers = 8;
+
+/* 4. Create a variable called division and initialise it with a value of 20 divided by 5. */
+var division = 20 / 5;
+
+/* 5. Write code that checks the type of the value "frog". */
+console.log(typeof "frog");
+
+/* 6. Declare and initialise a variable called orderHasShipped with a boolean value. */
+var orderHasShipped = true;
+
+/* 7. Create an if statement that checks if orderHasShipped is true. If it is true, console log the string value "true". If not, console log the string value "false". */
+if (orderHasShipped) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+/* 8. Create a for loop that counts from 0 to 9. Console log the value of the counter variable inside the loop. */
+for (i = 0; i <= 9; i++) {
+  console.log(i);
+}
