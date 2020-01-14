8f182fd267a129e9ea8d46490578861c81a3294c
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-01-14 13:51:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-1. Create an array called petArray with 3 string items in it: "cat", "cow", "dog".
+/*1. Create an array called petArray with 3 string items in it: "cat", "cow", "dog".*/
+var petArray = ["cat", "cow", "dog"];
 
+/*2. Console log the value of the second item in petArray.*/
+console.log(petArray[1]);
 
-2. Console log the value of the second item in petArray.
+/* 3. Add the item "sheep" to petArray using the push method. */
+petArray.push("sheep");
 
+/* 4. Console log how many items there are now in petArray using the length method. */
+console.log(petArray.length);
 
-3. Add the item "sheep" to petArray using the push method.
+/* 5. Create an object called catObject with 3 properties: name, colour, age. Add appropriate values (and type of values) for each of these properties. */
+var catObject = {
+  name: "Brutus",
+  colour: "Black",
+  age: "7"
+};
 
+/* 6. Console log the value of the third property of catObject. */
+console.log(catObject.age);
 
-4. Console log how many items there are now in petArray using the length method.
+/* 7. Take the value of catObject (the {} part) and place it inside an array called catArray. */
+var catArray = [catObject];
 
-5. Create an object called catObject with 3 properties: name, colour, age. Add appropriate values (and type of values) for each of these properties.
+/* 8. Loop throughcatArray and console log each property of each object inside (there will be only 1 object inside). */
+for (var count = 0; count < catArray.length; count++) {
+  console.log(catArray[count].name);
+  console.log(catArray[count].colour);
+  console.log(catArray[count].age);
+}
 
+/* 9. Create a function called logToConsole. The function should accept 1 argument. Decide on the name of the argument and console log its value inside the function. */
+function logToConsole(loggedtoconsole) {
+  console.log(loggedtoconsole);
+}
 
-6. Console log the value of the third property of catObject.
+/* 10. Call the logToConsole function and pass in the value "donkey". */
 
-7. Take the value of catObject (the {} part) and place it inside an array called catArray.
-
-
-8. Loop throughcatArray and console log each property of each object inside (there will be only 1 object inside).
-
-
-9. Create a function called logToConsole. Th function should accept 1 argument. Decide on the name of the argument and console log its value inside the function.
-
-
-10. Call the logToConsole function and pass in the value "donkey".
+logToConsole("Donkey");
